8e69e65b75eaed0f86e2579d6bcf994af77d86e4
Baraa <engbaraakhamaysa@gmail.com>
2025-09-16 21:03:20 +0300

    Add Verification Form SignUp

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,19 +1,66 @@
+import { useState } from "react";
+
 export default function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordR, setPasswordR] = useState("");
+  const [accept, setAccept] = useState(false);
+  console.log(name);
+
+  function Submit(e) {
+    e.preventDefault(); //Dont Can Sumbent The Form
+    setAccept(true);
+  }
   return (
     <div className="parent">
       <div className="register">
-        <form>
+        <form onSubmit={Submit}>
           <label htmlFor="name">Name:</label>
-          <input id="name" type="text" placeholder="Name..." />
+          <input
+            id="name"
+            type="text"
+            placeholder="Name..."
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          {name === "" && accept && (
+            <p className="error">User Name is Required</p>
+          )}
 
           <label htmlFor="email">Email:</label>
-          <input id="email" type="email" placeholder="Email..." required />
+          <input
+            id="email"
+            type="email"
+            placeholder="Email..."
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <label htmlFor="password">Password:</label>
-          <input id="password" type="password" placeholder="Password..." />
+          <input
+            id="password"
+            type="password"
+            placeholder="Password..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {password.length < 8 && accept && (
+            <p className="error">Password must be than 8 Char</p>
+          )}
 
           <label htmlFor="repeat">Repeat Password:</label>
-          <input id="repeat" type="password" placeholder="Repeat Password..." />
+          <input
+            id="repeat"
+            type="password"
+            placeholder="Repeat Password..."
+            value={passwordR}
+            onChange={(e) => setPasswordR(e.target.value)}
+          />
+          {passwordR !== password && accept && (
+            <p className="error">Password Dose Not Match</p>
+          )}
 
           <div style={{ textAlign: "center" }}>
             <button type="submit">Register</button>
