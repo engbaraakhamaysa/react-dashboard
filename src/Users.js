b71944c0dbef181002af76c7e304b69d130d5e09
Baraa <engbaraakhamaysa@gmail.com>
2025-09-19 07:57:22 +0300

    Add ShowUser& DeleteUser Api

--- src/Users.js
+++ src/Users.js
@@ -1,4 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8000/api/allusers")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      });
+  }, []);
+
+  function deleteUser(id) {
+    axios.delete(`http://localhost:8000/api/deleteUser/${id}`);
+  }
+
+  const showUsers = users.map((user, index) => (
+    <tr key={index}>
+      <td>{index + 1}</td>
+      <td>{user.name}</td>
+      <td>{user.email}</td>
+      <td>
+        <i
+          className="fa-solid fa-pen-to-square"
+          style={{ color: "#74afb9", fontSize: "20px", paddingRight: "40px" }}
+        ></i>
+        <i
+          onClick={() => deleteUser(user._id)}
+          className="fa-solid fa-trash"
+          style={{ color: "red", fontSize: "20px", cursor: "pointer" }}
+        ></i>
+      </td>
+    </tr>
+  ));
+
   return (
     <div style={{ padding: "20px" }}>
       <table>
@@ -6,14 +42,9 @@ export default function Users() {
           <th>ID</th>
           <th>Name</th>
           <th>Eamil</th>
+          <th>Action</th>
         </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Baraa</td>
-            <td>baraa@gmail.com</td>
-          </tr>
-        </tbody>
+        {showUsers}
       </table>
     </div>
   );
