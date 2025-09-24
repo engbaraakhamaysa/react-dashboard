692428598b2e97015e0993811e9ab33c46676c06
Baraa <engbaraakhamaysa@gmail.com>
2025-09-24 20:48:37 +0300

    Update Update User Page

--- src/Pages/Users/UpdateUser.js
+++ src/Pages/Users/UpdateUser.js
@@ -1,15 +1,29 @@
-import { useEffect, useState } from "react";
-import Form from "../../Components/Form/Form";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 
-export default function UpdateUser() {
-  const [name, setName] = useState("baraa");
-  const [email, setEmail] = useState("baraa@gmail");
+import { useNavigate } from "react-router-dom";
+import { User } from "../Website/Context/UserContext";
 
+export default function CreateUser() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordR, setPasswordR] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [accept, setAccept] = useState(false);
+
+  const context = useContext(User);
+  const token = context.auth.token;
+  const nav = useNavigate();
   const id = window.location.pathname.split("/").slice(-1)[0];
   console.log(id);
 
   useEffect(() => {
-    fetch(`http://localhost:8000/api/user/showuser/${id}`)
+    fetch(`http://localhost:8000/api/user/showuser/${id}`, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -18,23 +32,136 @@ export default function UpdateUser() {
       });
   }, [id]);
 
+  async function Submit(e) {
+    e.preventDefault();
+    setAccept(true);
+    try {
+      let res = await axios.put(
+        `http://localhost:8000/api/user/update/${id}`,
+        {
+          name: name,
+          email: email,
+          password: password,
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      console.log(res);
+
+      nav("/dashboard/users");
+    } catch (error) {
+      console.log(error);
+      if (error.response.status === 400) {
+        setEmailError(true);
+      }
+
+      setAccept(true);
+    }
+  }
   return (
-    <>
-      <h1>Update User</h1>
+    <div>
       <div>
-        <Form
-          button="Update"
-          name={name}
-          email={email}
-          action={`put`}
-          endPoint={`user/update/${id}`}
-          navigate="/dashboard/users"
-          hasLocalStorage={false}
-          styleReagister={false}
-          form={false}
-          buttonstyle={true}
-        />
+        <div>
+          <form onSubmit={Submit}>
+            <label htmlFor="name">Name:</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Name..."
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            {name.length < 2 && accept && (
+              <p className="error">Name must be more than 2 char</p>
+            )}
+
+            <label htmlFor="email">Email:</label>
+            <input
+              id="email"
+              type="email"
+              placeholder="Email..."
+              value={email}
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {accept && emailError && (
+              <p className="error">Email Is already benn token</p>
+            )}
+
+            <label htmlFor="password">Password:</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Password..."
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {password.length < 8 && accept && (
+              <p className="error">Password must be than 8 Char</p>
+            )}
+
+            <label htmlFor="repeat">Repeat Password:</label>
+            <input
+              id="repeat"
+              type="password"
+              placeholder="Repeat Password..."
+              value={passwordR}
+              onChange={(e) => setPasswordR(e.target.value)}
+            />
+            {passwordR !== password && accept && (
+              <p className="error">Password Dose Not Match</p>
+            )}
+
+            <div className="register button" style={{ textAlign: "center" }}>
+              <button type="submit">Update User</button>
+            </div>
+          </form>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
+
+// import { useEffect, useState } from "react";
+// import Form from "../../Components/Form/Form";
+
+// export default function UpdateUser() {
+//   const [name, setName] = useState("baraa");
+//   const [email, setEmail] = useState("baraa@gmail");
+
+//   const id = window.location.pathname.split("/").slice(-1)[0];
+//   console.log(id);
+
+//   useEffect(() => {
+//     fetch(`http://localhost:8000/api/user/showuser/${id}`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data);
+//         setName(data.name);
+//         setEmail(data.email);
+//       });
+//   }, [id]);
+
+//   return (
+//     <>
+//       <h1>Update User</h1>
+//       <div>
+//         <Form
+//           button="Update"
+//           name={name}
+//           email={email}
+//           action={`put`}
+//           endPoint={`user/update/${id}`}
+//           navigate="/dashboard/users"
+//           hasLocalStorage={false}
+//           styleReagister={false}
+//           form={false}
+//           buttonstyle={true}
+//         />
+//       </div>
+//     </>
+//   );
+// }
