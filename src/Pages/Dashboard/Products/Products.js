17e71607cf1d96e992545da666c9e5c06f837abf
Baraa <engbaraakhamaysa@gmail.com>
2025-09-26 23:55:14 +0300

    Edit Show All Product Page

--- src/Pages/Dashboard/Products/Products.js
+++ src/Pages/Dashboard/Products/Products.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { User } from "../../Website/Context/UserContext";
 
 export default function Products() {
-  const [users, setUsers] = useState([]);
+  const [products, setproducts] = useState([]);
   const [runUseEffect, setRenUseEffect] = useState(0);
 
   const context = useContext(User);
@@ -20,7 +20,7 @@ export default function Products() {
         },
       })
       .then((res) => {
-        setUsers(res.data);
+        setproducts(res.data);
       })
       .catch((err) => console.log(err));
   }, [runUseEffect]);
@@ -44,20 +44,20 @@ export default function Products() {
     }
   }
 
-  const showUsers = users.map((user, index) => (
+  const showProducts = products.map((product, index) => (
     <tr key={index}>
       <td>{index + 1}</td>
-      <td>{user.name}</td>
-      <td>{user.email}</td>
+      <td>{product.title}</td>
+      <td>{product.description}</td>
       <td>
-        <Link to={`${user._id}`}>
+        <Link to={`${product._id}`}>
           <i
             className="fa-solid fa-pen-to-square"
             style={{ color: "#74afb9", fontSize: "20px", paddingRight: "40px" }}
           ></i>
         </Link>
         <i
-          onClick={() => deleteUser(user._id)}
+          onClick={() => deleteUser(product._id)}
           className="fa-solid fa-trash"
           style={{ color: "red", fontSize: "20px", cursor: "pointer" }}
         ></i>
@@ -71,12 +71,12 @@ export default function Products() {
         <thead>
           <tr>
             <th>ID</th>
-            <th>Name</th>
-            <th>Eamil</th>
+            <th>Title</th>
+            <th>Description</th>
             <th>Action</th>
           </tr>
         </thead>
-        <tbody>{showUsers}</tbody>
+        <tbody>{showProducts}</tbody>
       </table>
     </div>
   );
