2b3c2cb05d7d43e59975ac3c436b6d7ed2c50ecc
Baraa <engbaraakhamaysa@gmail.com>
2025-09-26 23:52:03 +0300

    Add Show All Product Page

--- src/Pages/Dashboard/Products/Products.js
+++ src/Pages/Dashboard/Products/Products.js
@@ -1,3 +1,83 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { User } from "../../Website/Context/UserContext";
+
 export default function Products() {
-  return <div>Product</div>;
+  const [users, setUsers] = useState([]);
+  const [runUseEffect, setRenUseEffect] = useState(0);
+
+  const context = useContext(User);
+  const token = context.auth.token;
+  console.log(token);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/product/allproducts", {
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [runUseEffect]);
+
+  async function deleteUser(id) {
+    try {
+      const res = await axios.delete(
+        `http://localhost:8000/api/product/deleteproduct/${id}`,
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      if (res.status === 200) {
+        setRenUseEffect((prv) => prv + 1);
+      }
+    } catch {
+      console.log("Error");
+    }
+  }
+
+  const showUsers = users.map((user, index) => (
+    <tr key={index}>
+      <td>{index + 1}</td>
+      <td>{user.name}</td>
+      <td>{user.email}</td>
+      <td>
+        <Link to={`${user._id}`}>
+          <i
+            className="fa-solid fa-pen-to-square"
+            style={{ color: "#74afb9", fontSize: "20px", paddingRight: "40px" }}
+          ></i>
+        </Link>
+        <i
+          onClick={() => deleteUser(user._id)}
+          className="fa-solid fa-trash"
+          style={{ color: "red", fontSize: "20px", cursor: "pointer" }}
+        ></i>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Eamil</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>{showUsers}</tbody>
+      </table>
+    </div>
+  );
 }
